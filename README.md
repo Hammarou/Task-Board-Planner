a0fbfb5b3d3f4f5aed2ac48493b865e171fea48a
Hammarou <139810496+Hammarou@users.noreply.github.com>
2024-10-28 21:09:55 -0400

    new commits

--- README.md
+++ README.md
@@ -1,1 +1,81 @@
 # Task-Board-Planner
+
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [GitHub Deployment Location (HTTPS)](#github-deployment-location-https)
+- [GitHub Repository Location](#github-repository-location)
+- [Credits](#credits)
+- [Webpage Screenshots](#webpage-screenshots)
+- [License](#license)
+
+  
+## Description
+
+Greetings! 
+
+- The purpose of this project is to enable the intended user (a company associate/manager handling the employee payroll matrix) to view and manage employee payroll data. The user will be prompted to enter as many employees as they wish (they will be prompted to provide nominal background information about each employee, such as first and last names, and salary). From this, the user will be able to acquire a list of all the employees (along with their information) they have entered; as well as calculate the average employee salary.
+
+## Installation
+
+* Clone repository: `git clone git@github.com:Hammarou/Task-Board-Planner.git`
+* Inside terminal cd into cloned repository in local machine `cd Task-Board-Planner`
+* Install dependencies: `npm install` 
+
+## Usage
+
+- The purpose of the Task Board Planner is to allow users the ability to create and record project tasks; and assign each task a due date for completion. 
+
+- Tasks are stored locally and will continue across multiple user seesions, unless deleted.
+
+### Add task
+
+ For the successful utilization of this application:
+
+* Click 'Add Task' button
+* Complete all the fields in the pop-up modal 
+* Click the "Add Task" button in the pop-up
+
+### Task description
+
+Tasks that are saved to the 'To Do' task plate  have a title, due date and description based off of the inputs placed by the user in the modal form. Based of when each task is due, they are color coded:
+
+* White: The task is due at some date in the future of the current date.
+* Yellow: The task is due on the same day as the current date
+* Red: The task is overdue
+
+
+### Move task
+
+Each task can be moved between the three task plate ('To Do', In Progress' and 'Done') by drag and drop. However, once the task is placed in the 'Done' taskbar it changes color to white and can nolonger be dropped in the 'To Do' taskbar
+
+## Webpage Screenshot
+
+![]( /assets/screenshots/screenshot1.png "First screenshot")
+![](/assets/screenshots/screenshot2.png "Second screenshot")
+![](/assets/screenshots/screenshot3.png "Third screenshot")
+![](/assets/screenshots/screenshot4.png "Fourth screenshot")
+
+
+## GitHub Deployment Location (HTTPS)
+
+The deployed webpage can be viewed at following URL:
+
+
+
+
+## GitHub Repository Location
+
+
+
+
+## Credits
+
+N/A
+
+## License
+
+sPlease refer to the LICENSE in the repo.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,148 +1,259 @@
-let count = 0
-let taskId = 0
+// TODO: Ad comments explain what each line of code is doing
 
-// GENERATE UNIQUE TASK ID
-function generateTaskId() {
-    taskId++
-}
+const POLLING_DELAY = 500;
 
-let modal = new bootstrap.Modal(document.getElementById("formModal"), {});
-
-// SHOW MODEL WHEN BUTTON IS CLICKED
-$("#btn").click(function() {
-    modal.show()
-})
-
-// WHEN 'Add Task' (CLOSE) BUTTON IS CLICKED, HIDE MODEL AND HANDLE
-$("#close-btn").click(function() {
-    modal.hide()
-    handleAddTask(modal)   
-})
-
-// CREATE DELETE BUTTON
-function createBtn() {
-    let btn1 = $('<button>'+"Delete"+'</button>')
-    btn1.css("background-color", "red")
-
-return btn1
-}
+// VARIABLE TO HOLD THE MODAL INSTANCE
+let modal;
 
 // COMPARE WHETHER TWO DATES ARE THE SAME DAY
 function sameDay(d1, d2) {
-    return d1.year() === d2.year() &&
-      d1.date() === d2.date() &&
-      d1.day() === d2.day();
-  }
-
-// FUNCTION CREATES TASK CARD DIV
-function createDiv(task) {
-
-    let div = $('<div></div>')
-    
-    let para1 = createParagraph(task.Name)
-    let para2 = createParagraph(dayjs(task.DueDate).format("M/DD/YYYY"))
-    let para3 = createParagraph(task.Description)
-    div.append(para1)
-    div.append(para3)
-    div.append(para2)
-    let btn1 = createBtn()
-    createBtnListener(btn1, div)
-    div.append(btn1)
-    div.attr('id','dragdiv' + count)
-    condition = para2
-    condition = (condition.text())
-    condition = condition.toLowerCase()
-
-    const today = dayjs().format();
-    const target = dayjs(task.DueDate).format();
-
-
-// BACKGROUND COLOR OF TASK CARD IS SET BASED ON THE TASK DUE DATE
-    if(sameDay(dayjs(), dayjs(task.DueDate))) {
-        div.css("background-color", "yellow")
-        const ms_red = dayjs(task.DueDate).diff(dayjs(), "milliseconds") + 24 * 60 * 60 * 1000
-        setTimeout(function() {
-            if(div.parent().attr("id") == "done-cards") {
-                div.css("background-color", "red")
-                div.addClass("redBox")
-                div.removeClass("yellowBox")
-            }  
-        }, ms_red)
-        div.addClass("yellowBox")
-    }  else if( dayjs(task.DueDate).isBefore(dayjs()) ) {
-        div.css("background-color", "red")
-        div.addClass("redBox")
-    } else {
-        div.css("background-color", "white")
-        const ms_yellow = dayjs(task.DueDate).diff(dayjs(), "milliseconds")
-        setTimeout(function() {
-            if(div.parent().attr("id") == "done-cards") {
-                div.css("background-color", "yellow")
-                div.addClass("yellowBox")
-                div.removeClass("whiteBox")
-            }    
-        }, ms_yellow)
-        const ms_red = ms_yellow + 24 * 60 * 60 * 1000
-        setTimeout(function() {
-            if(div.parent().attr("id") == "done-cards") {
-                 div.css("background-color", "red")
-                div.addClass("redBox")
-                div.removeClass("yellowBox")
-            }
-        }, ms_red)
-
-        div.addClass("whiteBox")   
-    }
-    
-    createDraggable(div)
-
-    $("#todo-cards").append(div)
-    
-    count++  
+  return (
+    d1.year() === d2.year() && d1.date() === d2.date() && d1.day() === d2.day()
+  );
 }
 
-// FUNCTION MAKES DIV DRAGGABLE
-function createDraggable(div) {
-    
-    div.draggable({
-        revert: true,
-        stack: "#done-cards",
-        snap: ".snap-box",
-        start: function(event, ui) {
-            $(this).css("z-index", 1000)
-        },
-        stop: function(event, ui) {
-            $(this).css("z-index", 999)
-        }
-    });
-};
+function getTasks() {
+  return JSON.parse(localStorage.getItem("tasks")) || [];
+}
+
+function geTaskById(taskId) {
+  return getTasks()[taskId];
+}
+
+function saveTasks(tasks) {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// GENERATE UNIQUE TASK ID
+function generateTaskId() {
+  let nextId = JSON.parse(localStorage.getItem("nextId")) || 0;
+  const id = nextId++;
+  localStorage.setItem("nextId", JSON.stringify(nextId));
+  return id;
+}
 
 // ADD CLICK LISTENER TO BUTTON
 function createBtnListener(btn, div) {
-    btn.on("click", function() {
-        div.css("display", "none")
-    })
+  btn.on("click", function (event) {
+    handleDeleteTask(event);
+  });
 }
 
 function createParagraph(text) {
-    let para3 = $('<p>'+text+'</p>')
-    return para3
+  let para3 = $("<p>" + text + "</p>");
+  return para3;
 }
 
-// MAKE ELEMENTS WITH CLASS "dropTarget" DROPPABLE
-$('.dropTarget').droppable({
-    drop: function(ev, ui) {
-        $(ui.draggable).detach().css({top: 0, left: 0}).appendTo(this);
-        console.log($(this).attr("id"))
-        if($(this).attr("id") == "done-cards") {
-            console.log("Task complete!")
-           $(ui.draggable).css("background-color", "white") 
-            $(ui.draggable).addClass("whiteBox")
-           $(ui.draggable).removeClass("yellowBox redBox")
-        }          
+// CREATE DELETE BUTTON
+function createBtn() {
+  let btn1 = $("<button>" + "Delete" + "</button>");
+  btn1.css("background-color", "red");
+
+  return btn1;
+}
+
+// CREATE TASK CARD
+function createTaskCard(task, taskId) {
+  let card = $("<div></div>");
+  card.attr("data-task-id", taskId);
+
+  let para1 = createParagraph(task.name);
+  let para2 = createParagraph(dayjs(task.dueDate).format("M/DD/YYYY"));
+  let para3 = createParagraph(task.description);
+  card.append(para1);
+  card.append(para3);
+  card.append(para2);
+  let btn1 = createBtn();
+  createBtnListener(btn1, card);
+  card.append(btn1);
+  card.attr("id", "dragdiv" + taskId);
+  condition = para2;
+  condition = condition.text();
+  condition = condition.toLowerCase();
+
+  const today = dayjs().format();
+  const target = dayjs(task.DueDate).format();
+
+  createDraggable(card);
+
+  return card;
+}
+
+// FUNCTION MAKES DIV DRAGGABLE
+function createDraggable(card) {
+  $(card).draggable({
+    revert: true,
+    stack: "#done-cards",
+    snap: ".snap-box",
+    start: function (event, ui) {
+      $(this).css("z-index", 1000);
+    },
+    stop: function (event, ui) {
+      $(this).css("z-index", 999);
+    },
+  });
+}
+
+function setCardColor(card, task) {
+  const dueDate = dayjs(task.dueDate)
+  const today = dayjs()
+  if(task.status == 'done') {
+    card.addClass("whiteBox");
+    card.removeClass("redBox yellowBox");
+  } else {
+    // BACKGROUND COLOR OF TASK CARD IS SET BASED ON THE TASK DUE DATE
+    if (sameDay(dueDate, today)) {
+      card.addClass("yellowBox");
+      card.removeClass("redBox whiteBox");
+    } else if (today.isAfter(dueDate)) {
+      card.addClass("redBox");
+      card.removeClass("whiteBox yellowBox");
+    } else {
+      card.addClass("whiteBox");
+      card.removeClass("redBox yellowBox");
     }
+  }
+}
+
+function colorAllCards() {
+  const todoSwimLane = $("#todo-cards");
+  const inProgressSwimLane = $("#in-progress-cards");
+  const doneSwimLane = $("#done-cards");
+  const cardList = [
+    ...todoSwimLane.children(),
+    ...inProgressSwimLane.children(),
+    ...doneSwimLane.children()
+  ];
+ 
+  // RETRIEVE TASKS AND nextId FROM localStorage
+  const taskList = getTasks();
+  
+  // RECOLOR ALL TASKS
+  for(let el of cardList) {
+    const card = $(el);
+    const taskId = +card?.data('task-id');
+    const task = taskList[taskId];
+    setCardColor(card, task);
+  }
+}
+
+// RENDER TASK LIST AND MAKE CARDS DRAGGABLE
+function renderTaskList() {
+  // Retrieve tasks and nextId from localStorage
+  let taskList = getTasks();
+  console.log({taskList}) 
+  const todoSwimLane = $("#todo-cards");
+  const inProgressSwimLane = $("#in-progress-cards");
+  const doneSwimlane = $("#done-cards");
+  todoSwimLane.empty();
+  inProgressSwimLane.empty();
+  doneSwimlane.empty();
+  const numTasks = taskList.length;
+  
+  for (let i = 0; i < numTasks; i++) {
+    const task = taskList[i];
+    const card = createTaskCard(task, i);
+    card.data("task-id", i);
+  
+    switch(task.status) {
+      case "todo":
+        todoSwimLane.append(card);
+        break;
+      case "in-progress":
+        inProgressSwimLane.append(card);
+        break;
+      case "done":
+        doneSwimlane.append(card);
+        break;
+    }
+    
+    colorAllCards();
+  }
+}
+
+// FUNCTION TO HANDLE ADDING A NEW TASK
+function handleAddTask(event) {
+  const task = {
+    id: generateTaskId(),
+    name: $("#task-name").val(),
+    dueDate: $("#due-date").val(),
+    description: $("#task-description").val(),
+    status: 'todo'
+  };
+  // RETRIEVE TASKS AND nextId FROM localStorage
+  let taskList = getTasks();
+  taskList.push(task);
+  saveTasks(taskList);
+  renderTaskList();
+}
+
+// DELETE TASKS
+function handleDeleteTask(event) {
+  const taskId = +event.target.parentNode.dataset.taskId;
+  // RETRIEVE TASKS AND nextId FROM localStorage
+  let taskList = getTasks();
+  tasklist = taskList.splice(taskId, 1);
+  saveTasks(taskList);
+  renderTaskList();
+}
+
+// DROP TASK INTO NEW STATUS LANE
+function handleDropx(event, ui) {
+  console.log({this:this, event: event})
+  renderTaskList();
+}
+
+/* UPON PAGE LOADING:
+      TASK LIST IS RENDERED 
+      EVENT LISTENERS ADDED,
+      LANES MADE DROPPABLE,
+      DUE DATE FIELD IS MADE INTO A DATE PICKER
+*/
+
+$(document).ready(function () {
+  modal = new bootstrap.Modal(document.getElementById("formModal"), {});
+
+  // SHOW MODEL WHEN BUTTON IS CLICKED
+  $("#btn").click(function () {
+    modal.show();
+  });
+
+  // WHEN 'Add Task' (CLOSE) BUTTON IS CLICKED, HIDE MODEL AND HANDLE
+  $("#close-btn").click(function () {
+    modal.hide();
+    handleAddTask(modal);
+  });
+
+  renderTaskList();
+
+  // MAKE ELEMENTS WITH CLASS "dropTarget" DROPPABLE
+  $(".dropTarget").droppable({
+    greedy: true,
+    drop: function (ev, ui) {
+      console.log('drop', {dropTarget:this, ev, ui})
+      const card = ui.draggable;
+      let taskList = getTasks();
+      const taskId = +card.data('task-id');
+      const task = taskList[taskId];
+      switch($(this).attr("id")) {
+        case "todo-cards":
+          task.status = 'todo';
+          break;
+        case "in-progress-cards":
+          task.status = 'in-progress';
+          break;
+        case "done-cards":
+          task.status = 'done';
+          console.log(`Task ${taskId} is Complete!`);
+      };
+      card.detach().appendTo(this);
+      saveTasks(taskList);
+      renderTaskList()
+    }
+  });
+
+  setInterval(()=>{
+    colorAllCards();
+  }, POLLING_DELAY)
+  
 });
-
-
-
-
